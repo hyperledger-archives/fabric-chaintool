3c23654289a580b3402cc9af2bc7cc432fedd86d
Gregory Haskins <gregory.haskins@gmail.com>
2017-02-01 22:18:15 -0500

    Import release v0.9.1
    This is a legacy import of the release originally managed in pure
    github.  You may find the history of this release in the archives
    here:
    
    https://github.com/hyperledger-archives/fabric-chaintool/releases/tag/v0.9.1
    
    Change-Id: Ifc3fb8e25e3d2fb1c429cea8119c357ce9fbd7b3
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chaintool "0.9.0"
+(defproject chaintool "0.9.1"
   :description "hyperledger chaincode tool"
   :url "https://github.com/ghaskins/chaintool"
   :license {:name "Apache License"
@@ -6,6 +6,7 @@
   :min-lein-version "2.0.0"
   :lein-release {:deploy-via :shell :shell ["true"]}
   :javac-options ["-target" "1.7" "-source" "1.7"]
+  :jvm-opts ["-server"]
   :java-source-paths ["src"]
   :plugins [[lein-bin "0.3.5"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
