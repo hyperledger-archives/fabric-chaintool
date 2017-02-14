b579a6a72e5915905c70b719d31b874155969f7d
Gregory Haskins <gregory.haskins@gmail.com>
2017-02-14 11:00:47 -0500

    Release v0.10.2
    Changes since v0.10.1:
    
    708af63 Add basic support for eventhub
    62f24c2 Remove base64 from protocol
    
    Change-Id: Ibe207c345bd6c7e1ee3385544a0bfdde3c0e8aba
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chaintool "0.10.2-SNAPSHOT"
+(defproject chaintool "0.10.2"
   :description "Hyperledger Fabric chaincode tool"
   :url "https://github.com/hyperledger/fabric-chaintool"
   :license {:name "Apache License"
