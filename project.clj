d597d9340f0c08ccae943d4efefe318e769faea3
Greg Haskins <gregory.haskins@gmail.com>
2018-06-12 21:59:17 -0400

    [FAB-10616] Prepare for v1.1.3 development
    Change-Id: I842084958fbf504fb96fcda067d751a361fa9612
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 ;; SPDX-License-Identifier: Apache-2.0
 ;;
 
-(defproject chaintool "1.1.2"
+(defproject chaintool "1.1.3-SNAPSHOT"
   :description "Hyperledger Fabric chaincode tool"
   :url "https://github.com/hyperledger/fabric-chaintool"
   :license {:name "Apache License"
